4d9b3b8c4f7f79a60ca933b6b7afbdd2f822eeab
Abror <abrorhalimov660@gmail.com>
2025-02-22 23:17:52 +0500

    Add project files.

---
+++ Crud_Home/Models/Home.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud_Home.Models;
+
+public class Home
+{
+
+    public Guid Id { get; set; }
+    public string Location { get; set; }
+    public List<string> Rooms { get; set; } = new List<string>();
+    public double Price { get; set; }
+    public int Floor { get; set; }
+
+}
