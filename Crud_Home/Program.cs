8dc9c27781e06b36e3c52ff70f822ad5da4c0316
Abror <abrorhalimov660@gmail.com>
2025-02-22 23:51:46 +0500

    Frontend

--- Crud_Home/Program.cs
+++ Crud_Home/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Crud_Home
+﻿using Crud_Home.Services;
+
+namespace Crud_Home
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             
+        }
+
+        public static void FrontEnd()
+        {
+            var homeService = new HomeService();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Home");
+                Console.WriteLine("2. Get Home By Id");
+                Console.WriteLine("3. Delete Home");
+                Console.WriteLine("4. Update Home");
+                Console.WriteLine("5. Get All Homes");
+                Console.WriteLine("6. Get Homes By Price");
+                Console.WriteLine("7. Get Home By Floor");
+                Console.WriteLine("8. Buy Homy By Id");
+            }
         }
     }
 }
